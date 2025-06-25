fbaeb639b828939b2b13bd9cb24385d84a39e08a
David Waligora <davidwaligora@hotmail.com>
2025-06-25 16:36:47 +0200

    push

--- linkto/next.config.js
+++ linkto/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static HTML export in Next.js 13+
+    basePath: '/LinkTo', // ⚠️ replace with your GitHub repo name
+  trailingSlash: true,
 };
 module.exports = nextConfig;
